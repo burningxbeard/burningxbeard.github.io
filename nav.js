a12da9d746ebb5af76c21bfa4fdcd2f84063d034
Chris Braid <chrisbraid.dev@gmail.com>
2024-07-02 16:46:20 -0400

    tidying up the javascript

--- nav.js
+++ nav.js
@@ -1,6 +1,14 @@
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-menu-toggle");
 
+
+window.addEventListener('load', () => {
+    window.history.pushState('','','/',('#'),[0]);
+    }); 
+    window.addEventListener('hashchange', () => {
+    window.history.pushState('','','/',('#'),[0]);
+    });
+
 navToggle.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute('data-visible');
     if (visibility === "false") {
@@ -16,3 +24,4 @@ document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", 
     navToggle.setAttribute('aria-expanded', false);
 }))
 });
+
